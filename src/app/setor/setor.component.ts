7686cdf370d636f2e65768baf1049d7129dd5299
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2020-03-05 15:32:36 -0300

    Primeiro commit

---
+++ src/app/setor/setor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setor',
+  templateUrl: './setor.component.html',
+  styleUrls: ['./setor.component.css']
+})
+export class SetorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
